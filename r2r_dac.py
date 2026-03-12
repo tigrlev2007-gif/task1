e1824237fd726dc86167f403b1bfe309d8e35ad4
ranker <tigrlev2007@gmail.com>
2026-03-12 12:59:39 +0300

    Create r2r_dac.py

---
+++ r2r_dac.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+
+    def set_number(self, number):
+        return [int(bit) for bit in bin(number)[2:].zfill(8)]
+
+    def set_voltage(self, voltage):
+        if voltage < 0: voltage = 0
+        if voltage > self.dynamic_range: voltage = self.dynamic_range
+        number = int(voltage / self.dynamic_range * 255)
+        bit_list = self.set_number(number)
+        GPIO.output(self.gpio_bits, bit_list)
+
+if __name__ == "__main__":
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
